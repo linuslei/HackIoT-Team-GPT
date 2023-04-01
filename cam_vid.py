cf1b91822ca73411fcdf459e784a489b0b1bca13
kevinjhluo <97573036+kevinjhluo@users.noreply.github.com>
2023-04-01 16:29:31 -0700

    footage capturing

--- cam_vid.py
+++ cam_vid.py
@@ -6,4 +6,6 @@ picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 
-picam2.start_and_record_video("test.mp4", duration = 10)
+def start_recording(crash_bool):
+	if crash_bool:
+		picam2.start_and_record_video("footage.mp4", duration = 10)
