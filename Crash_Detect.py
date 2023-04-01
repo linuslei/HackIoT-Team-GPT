e8a73c08c38bc787700abc8b3fa1329d86323722
linuslei <linuslei@usc.edu>
2023-04-01 16:45:25 -0700

    crash detect

--- Crash_Detect.py
+++ Crash_Detect.py
@@ -1,9 +1,15 @@
 #Detect Crash
 
+#Notice: Wiring CLK = 11 MISO = 9 MOSI = 10 CS = 8
 import time
 import board
 import busio
 import adafruit_mpu6050
+import RPi.GPIO as GPIO
+import Adafruit_MCP3008
+import numpy as np
+from Body_Detect import read_pir_sensor
+
 
 #Use MPU6050 to detect crash
 #A crash is defined as a sudden change in acceleration
@@ -11,7 +17,7 @@ import adafruit_mpu6050
 #The accelerometer measures acceleration in 3 axes
 #The gyroscope measures angular velocity in 3 axes
 
-#TODO:Read the accelerometer data, Write it as a function
+#Read the accelerometer data, Write it as a function
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_mpu6050.MPU6050(i2c)
 
@@ -26,6 +32,25 @@ def read_accel():
     accel_y = accel_y / 9.8
     accel_z = accel_z / 9.8
     return accel_x, accel_y, accel_z
+
+def read_sound():
+    CLK = 11
+    MISO = 9
+    MOSI = 10
+    CS = 8
+    mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+    analog_value = mcp.read_adc(0)
+    return analog_value
+
+def detect_crash(accel_thres, sound_thres):
+    accel_x,accel_y,accel_z = read_accel()
+    sound = read_sound()
+    abs_accel = np.sqrt(accel_x**2 + accel_y**2 + accel_z**2)
+    if abs_accel > accel_thres or sound > sound_thres:
+        crash_flag = 1
+    else:
+        crash_flag = 0
+    return crash_flag, abs_accel, sound
 
 
 
