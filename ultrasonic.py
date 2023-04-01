cb3619d02e145cdcc55ed4203c202cd5b28f33d3
kevinjhluo <97573036+kevinjhluo@users.noreply.github.com>
2023-04-01 16:29:04 -0700

    distance sensing

--- ultrasonic.py
+++ ultrasonic.py
@@ -14,30 +14,12 @@ def distance():
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
 
-    # save StartTime
     while GPIO.input(GPIO_ECHO) == 0:
         start_time = time.time()
- 
-    # save time of arrival
     while GPIO.input(GPIO_ECHO) == 1:
         stop_time = time.time()
- 
-    # time difference between start and arrival
-    duration = Stop_time - Start_time
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = duration * 17150
+    duration = stop_time - start_time
+    distance = duration * 17150  #17150=34300(speed of sound)/2(back and forth)
  
     return distance
  
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
