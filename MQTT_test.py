cde6cd62e7c730e167fa9a5cb7f88945d5d7a6d2
cwu67414 <cwu67414@usc.edu>
2023-04-01 16:00:20 -0700

    test

--- MQTT_test.py
+++ MQTT_test.py
@@ -1,53 +1,62 @@
+from flask import Flask, render_template
 import paho.mqtt.client as mqtt
-import time
 
-#Create an instance of the MQTT client and set up the connection to the MQTT broker
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Set up the connection to the MQTT broker
 client = mqtt.Client()
 client.connect("test.mosquitto.org", 1883, 60)
 
-#Define the MQTT topics to be used
+# Define the MQTT topics to be used
 accelerometer = "crash_detect/accelerometer"
 sound = "crash_detect/microphone_sensor"
 motion = "crash_detect/motion_sensor"
 vision = "crash_detect/camera_sensor"
 
+# Define the initial state of the variables
+accelerometer_state = ""
+sound_state = ""
+motion_state = ""
+vision_state = ""
 
-#Define the initial state of the light bulb
-state = "OFF"
-brightness = 0
-
-#Define the callback function for the MQTT client
+# Define the callback function for the MQTT client
 def on_message(client, userdata, message):
-    global accelerometer
-    global sound
-    global motion
-    global vision
+    global accelerometer_state
+    global sound_state
+    global motion_state
+    global vision_state
     print("Received message '" + str(message.payload) + "' on topic '" + message.topic + "' with QoS " + str(message.qos))
     if message.topic == accelerometer:
-        accelerometer = str(message.payload.decode("utf-8"))
+        accelerometer_state = str(message.payload.decode("utf-8"))
     elif message.topic == sound:
-        sound = str(message.payload.decode("utf-8"))
+        sound_state = str(message.payload.decode("utf-8"))
     elif message.topic == motion:
-        motion = str(message.payload.decode("utf-8"))
+        motion_state = str(message.payload.decode("utf-8"))
     elif message.topic == vision:
-        vision = str(message.payload.decode("utf-8"))
+        vision_state = str(message.payload.decode("utf-8"))
 
-#Set the callback function for the MQTT client
+# Set the callback function for the MQTT client
 client.on_message = on_message
 
-#Subscribe to the MQTT topics
+# Subscribe to the MQTT topics
 client.subscribe(accelerometer)
 client.subscribe(sound)
 client.subscribe(motion)
 client.subscribe(vision)
 
-#Start the MQTT client loop
+# Start the MQTT client loop
 client.loop_start()
 
-#Main loop
-while True:
-    #Publish the state and brightness of the light bulb
-    client.publish("crash_detect/accelerometer", accelerometer)
-    client.publish("crash_detect/microphone_sensor", sound)
-    client.publish("crash_detect/motion_sensor", motion)
-    time.sleep(1)
+# Define a route for the index page
+@app.route('/')
+def index():
+    global accelerometer_state
+    global sound_state
+    global motion_state
+    global vision_state
+    return render_template('index.html', accelerometer=accelerometer, sound=sound, motion=motion, vision=vision)
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
