77ebdd5bc5026b93304765dd84185ed201c098b6
kevinjhluo <luok@usc.edu>
2023-04-01 14:57:34 -0700

    rpi camera module 2

--- cam_vid.py
+++ cam_vid.py
@@ -7,8 +7,6 @@ picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 
-encoder = H264Encoder(10000000)
-
-picam2.start_recording(encoder, 'test.h264')
+picam2.start_recording('test.mp4')
 time.sleep(10)
 picam2.stop_recording()
