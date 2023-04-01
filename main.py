d4253d3e1a3bc7925bad27ed4c2e40c1a83f8eb7
linuslei <linuslei@usc.edu>
2023-04-01 14:35:15 -0700

    update

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #Main program
+
+# Set up an interrupt function that will trigger when the accelerometer detects a sudden increase in acceleration or the microphone sensor detects a loud sound.
+
+# The interrupt function will then trigger the crash detection function.
+
+# The crash detection function will then trigger the buzzer and the LED to flash.
+
+# The crash detection function will also trigger the camera to take a picture and save it to the SD card.
+
+# The crash detection function will also trigger the GPS to record the location of the crash.
+
+# The crash detection function will also trigger the accelerometer to record the acceleration of the crash.
+
+# The crash detection function will also trigger the microphone to record the sound of the crash.
+
+# The crash detection function will also trigger the temperature sensor to record the temperature of the crash.
+
+# The crash detection function will also trigger the humidity sensor to record the humidity of the crash.
+
+# The crash detection function will also trigger the pressure sensor to record the pressure of the crash.
+
+# The crash detection function will also trigger the light sensor to record the light of the crash.
