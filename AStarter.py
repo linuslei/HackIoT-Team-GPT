b5e5641c8b68fa0c6da3322b56e4560dbc0b7eba
linuslei <linuslei@usc.edu>
2023-04-01 20:30:28 -0700

    test MQTT

--- AStarter.py
+++ AStarter.py
@@ -1,9 +1,30 @@
 from Body_Detect import read_pir_sensor
 from Crash_Detect import detect_crash
+from cam_vid import start_recording
+from ultrasonic import distance
+import time
+import paho.mqtt.client as mqtt
 
+BROKER = 'test.mosquitto.org'
+PORT = 1883
+
+client = mqtt.Client()
+client.connect(BROKER, PORT)
+print(f"Connected to MQTT broker: {BROKER}")
 
 pir_sensor_pin = 11
 motion_detected = read_pir_sensor(pir_sensor_pin)
 
 if motion_detected:
-   crash_flag = detect_crash(1,200)
+    while crash_flag == 0:
+        crash_flag, accel, sound = detect_crash(1, 200)
+        if crash_flag == 1:
+            #start_recording()
+            client.publish("crash_detect/crash_flag", crash_flag)
+            client.publish("crash_detect/accelerometer", accel)
+            client.publish("crash_detect/microphone_sensor", sound)
+         #Publish to MQTT Upload to Amazon Cloud
+         
+
+            
+
